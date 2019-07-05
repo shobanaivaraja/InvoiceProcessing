1c99b4e22205337a139493a9b0418f0118663698
Shobana Devi <36257085+shobanaivaraja@users.noreply.github.com>
2019-07-05 15:46:16 +0530

    Add files via upload

---
+++ src/business/TextConverter.java
@@ -0,0 +1,79 @@
+package business;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import database.AddInvoice;
+
+public class TextConverter implements AddInvoice{
+	
+	void convertToTextFile(String fileName) throws FileNotFoundException {
+		File pdfFileName = null;
+		PDDocument document = null;
+		PrintWriter writer = null;
+		File textFile = null;
+		BufferedReader buffer = null;
+		try {
+			String file = "D:/attachments/" + fileName;
+			pdfFileName = new File(file);
+			document = PDDocument.load(pdfFileName); // put path to your input pdf file here
+			String text = new PDFTextStripper().getText(document);
+			textFile = new File("D:/invoice.txt");
+			writer = new PrintWriter(new FileWriter(textFile, false));
+			writer.write(text);
+			writer.close();
+			buffer = new BufferedReader(new FileReader("D:/invoice.txt"));
+			String line = "";
+			String invoiceDate = "", total = "", customerPO = "", address = "";
+			long invoiceNo = 0;
+			buffer.mark(4048);
+			line = buffer.readLine();
+			while (line != null) {
+				String[] words = line.split("\n");
+				for (String word : words) {
+					if (word.equals("Invoice Date")) {
+						invoiceDate = buffer.readLine();
+						// System.out.println("Date"+invoiceDate);
+					}
+					if (word.equals("Invoice No")) {
+						invoiceNo = Long.parseLong(buffer.readLine());
+						// System.out.println("Date"+invoiceNo);
+					}
+					if (word.equals("Customer P.O.")) {
+						customerPO = buffer.readLine();
+						// System.out.println("Date"+customer);
+					}
+					if (word.equals("Sold To")) {
+						address = buffer.readLine() + buffer.readLine() + buffer.readLine();
+						// System.out.println("Date"+address);
+					}
+					if (word.startsWith("$")) {
+						total = word;
+						// System.out.println("Date"+total);
+					}
+				}
+
+				line = buffer.readLine();
+			}
+			AddInvoice.super.insertToDb(invoiceDate, invoiceNo, customerPO, total, address);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				document.close();
+				writer.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
